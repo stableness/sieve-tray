e0847f6d7e228861fe2376123a01d807a8166b9c
Cotton Hou <himcotton@gmail.com>
2019-12-25 20:41:16 +0800

    add ci.sh

---
+++ ci.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -eu
+
+
+
+gen_china_list()
+{
+
+    OUTPUT=output.txt
+    BASE=https://cdn.jsdelivr.net/gh/felixonmars/dnsmasq-china-list
+
+    echo '' > $OUTPUT.raw
+    curl -sSL $BASE/apple.china.conf >> $OUTPUT.raw
+    curl -sSL $BASE/google.china.conf >> $OUTPUT.raw
+    # curl -sSL $BASE/accelerated-domains.china.conf >> $OUTPUT.raw
+
+    awk -F '/' 'NF {print $2}' $OUTPUT.raw | sort -b -f | uniq -i > $OUTPUT
+
+    mkdir -p json
+    wc -l $OUTPUT | xargs node gen.js > json/china-list.json
+
+}
+
+
+
+cd $1 && shift && "$@"
+
